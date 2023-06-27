47da2d33e2c4740c839ccafe74bbd9e7253645f7
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-06-27 14:43:36 -0700

    learned about scroll

---
+++ Scroll Indicator/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("scroll",()=> {
+	let scrollheight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (window.scrollY / scrollheight) * 100;
+  document.querySelector(".progress").style.width = scrolled + "%";
+});
