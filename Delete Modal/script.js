6dd898a123ed4630dbffbca3de207a43b50071ea
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-06-27 13:59:26 -0700

    delete modal

--- Delete Modal/script.js
+++ Delete Modal/script.js
@@ -2,6 +2,10 @@ document.querySelector(".openmodal").addEventListener("click", () => {
 	document.querySelector(".modal").style.display = "block";
 });
 
-document.querySelector(".modal-close").addEventListener("click", () => {
+document.querySelector(".can").addEventListener("click", () => {
+	document.querySelector(".modal").style.display = "none";
+});
+
+document.querySelector(".del").addEventListener("click", () => {
 	document.querySelector(".modal").style.display = "none";
 });
