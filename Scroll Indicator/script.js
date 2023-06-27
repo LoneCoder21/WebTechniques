62977f7738c6d581b2cffa195a75cb9ab5b9006e
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-06-27 15:01:57 -0700

    percentage indicator

--- Scroll Indicator/script.js
+++ Scroll Indicator/script.js
@@ -2,4 +2,5 @@ document.addEventListener("scroll",()=> {
 	let scrollheight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 	var scrolled = (window.scrollY / scrollheight) * 100;
   document.querySelector(".progress").style.width = scrolled + "%";
+  document.querySelector(".progress-text").innerText = Math.floor(scrolled) + " %";
 });
