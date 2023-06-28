cb60dccb73de57980fc617c052467da79e9bf59f
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-06-28 00:56:04 -0700

    notifier snackbar

---
+++ Notification Snackbar/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".notify").addEventListener("click",()=>{
+	let notification = document.querySelector(".notification").style;
+	notification.display = "block";
+	setTimeout(()=>{
+		notification.display = "none";
+	},3000);
+});
