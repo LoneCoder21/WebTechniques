b447c53e75635bd49c22a6f785b70c35b2cdf58c
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-06-28 16:25:17 -0700

    quotes slideshow with js

---
+++ Quotes Slideshow/script.js
@@ -0,0 +1,22 @@
+let quote = 0;
+
+function activateQuoteSection() {
+	let quotes = document.querySelectorAll(".quotesection");
+	for (q of quotes) {
+		q.classList.remove("active");
+	}
+	quotes[quote].classList.add("active");
+}
+
+document.querySelector(".Left").addEventListener("click",()=>{
+	quote-=1;
+	quote+=3;
+	quote%=3;
+	activateQuoteSection();
+});
+
+document.querySelector(".Right").addEventListener("click",()=>{
+	quote+=1;
+	quote%=3;
+	activateQuoteSection();
+});
